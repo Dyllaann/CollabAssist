b8b4a2d08e86f3462a31722a0844a1b7f29f2a8d
Dyllaann <dylan.vanwaarden@gmail.com>
2020-10-22 20:04:53 +0200

    Fix startup and add configuration injection

---
+++ src/CollabAssist.Configuration/CollabAssistConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Security;
+
+namespace CollabAssist.Configuration
+{
+    public class CollabAssistConfiguration
+    {
+        public Auth Auth { get; set; }
+    }
+
+    public class Auth
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
