965a9df0d00397e1515f8b78a0848b20b403f88a
Dyllaann <dylan.vanwaarden@gmail.com>
2021-04-26 14:58:50 +0200

    Add health endpoint for k8s monitoring

---
+++ src/CollabAssist.API/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollabAssist.API.Controllers
+{
+    [Route("api/health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        [Route("status")]
+        public IActionResult GetStatus()
+        {
+            return StatusCode(StatusCodes.Status200OK);
+        }
+    }
+}
