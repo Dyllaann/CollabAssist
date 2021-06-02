7ac974e290d4fa322d56bb602deefaa68e5eed41
Dyllaann <dylan.vanwaarden@gmail.com>
2021-06-02 21:37:33 +0200

    Encode DevOps PR url

--- src/CollabAssist.Incoming.DevOps/DevOpsUtils.cs
+++ src/CollabAssist.Incoming.DevOps/DevOpsUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.RegularExpressions;
+using System.Web;
 using CollabAssist.Incoming.DevOps.Models;
 
 namespace CollabAssist.Incoming.DevOps
@@ -18,7 +19,8 @@ namespace CollabAssist.Incoming.DevOps
                 var repository = pr.Resource.Repository.Name;
                 var id = pr.Resource.PullRequestId;
 
-                return $"https://dev.azure.com/{org}/{project}/_git/{repository}/pullrequest/{id}";
+                var url = $"https://dev.azure.com/{org}/{project}/_git/{repository}/pullrequest/{id}";
+                return HttpUtility.UrlEncode(url);
             }
 
             return null;
